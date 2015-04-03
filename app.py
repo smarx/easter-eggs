af13994fb9045e6e362b9ddde8160461ddfdfdca
Steve Marx <smarx@smarx.com>
2015-04-02 18:47:13 -0700

    miscellaneous cleanup

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from hashlib import sha256
-import hmac
 import json
 import os
 import os.path
-import threading
-import urlparse
 import random
 import time
+import urlparse
 
 from dropbox.client import DropboxClient, DropboxOAuth2Flow
 from flask import abort, Flask, redirect, render_template, request, session, url_for
@@ -26,8 +23,13 @@ app.debug = True
 app.secret_key = os.environ['FLASK_SECRET_KEY']
 
 # Easter variables
-# TODO copy ref paths
-# TODO start time
+egg_refs = {
+    'egg.jpg': 'M7Zx2HpjY285MDRrbXlrdg',
+    'blue_egg.jpg': 'M7Zx2DN5Nm05M3Zqbmh3bA',
+    'pastel_egg.jpg': 'M7Zx2Ho4d2w1d3B6czRubQ',
+    'polka_dot_egg.jpg': 'M7Zx2DQ0YnFmMGwxcWZ0aA',
+    'nest_eggs.jpg': 'M7Zx2HByb25hbmhvcXh0dA',
+}
 
 def get_url(route):
     '''Generate a proper URL, forcing HTTPS if not running locally'''
@@ -48,11 +50,6 @@ def get_flow():
         session,
         'dropbox-csrf-token')
 
-@app.route('/welcome')
-def welcome():
-    return render_template('welcome.html', redirect_url=get_url('oauth_callback'),
-        webhook_url=get_url('webhook'), home_url=get_url('index'), app_key=APP_KEY)
-
 @app.route('/oauth_callback')
 def oauth_callback():
     '''Callback function for when the user returns from OAuth.'''
@@ -63,12 +60,7 @@ def oauth_callback():
     # Extract and store the access token for this user
     redis_client.hset('tokens', uid, access_token)
 
-    process_user(uid)
-
-    return redirect(url_for('done'))
-
-def process_user(uid):
-    '''Create folder tree, put random easter eggs in it.'''
+    # Create folder tree, put random easter eggs in it.
 
     # OAuth token for the user
     token = redis_client.hget('tokens', uid)
@@ -90,43 +82,35 @@ def process_user(uid):
 
     client.file_create_folder('/Easter basket')
 
-    hide_eggs(uid)
-    redis_client.hset('start_times',uid, time.time())
-
-def hide_eggs(uid):
-    token = redis_client.hget('tokens', uid)
-
-    client = DropboxClient(token)
-
     # Get flat list of paths to directories from '/Yard'
-    flat_list = {}
-    recurse_yard('/Yard',flat_list,client)
+    flat_list = enumerate_yard('/Yard', client)
 
     # Choose 5 random places to hide eggs
-    hiding_places = random.sample(set(flat_list), 5)
-    print hiding_places
+    hiding_places = random.sample(flat_list, 5)
 
     # copy egg images into those hiding places
-    egg_refs = {
-        'egg.jpg': 'M7Zx2HpjY285MDRrbXlrdg',
-        'blue_egg.jpg': 'M7Zx2DN5Nm05M3Zqbmh3bA',
-        'pastel_egg.jpg': 'M7Zx2Ho4d2w1d3B6czRubQ',
-        'polka_dot_egg.jpg': 'M7Zx2DQ0YnFmMGwxcWZ0aA',
-        'nest_eggs.jpg': 'M7Zx2HByb25hbmhvcXh0dA',
-    }
     keys = egg_refs.keys()
 
     for i in range(5):
         client.add_copy_ref(egg_refs[keys[i]], hiding_places[i] + '/' + keys[i])
 
-def recurse_yard(path, list, client):
-    contents = client.metadata(path)['contents']
-    if (len(contents) > 0):
-        for item in contents:
-            if item['is_dir']:
-                path = item['path']
-                list[path] = path
-                recurse_yard(path, list, client)
+    redis_client.hset('start_times', uid, time.time())
+
+    return redirect(url_for('done'))
+
+def enumerate_yard(path, client):
+    cursor = None
+    has_more = True
+
+    paths = set()
+
+    while has_more:
+        response = client.delta(path_prefix=path, cursor=cursor)
+        for path, metadata in response['entries']:
+            paths.add(path)
+        has_more = response['has_more']
+
+    return paths
 
 @app.route('/')
 def index():
@@ -148,27 +132,16 @@ def check_basket():
 
     client = DropboxClient(token)
 
-    file_names = {'egg.jpg','blue_egg.jpg','pastel_egg.jpg','polka_dot_egg.jpg','nest_eggs.jpg'}
+    # check if /Easter basket has all five eggs
+    basket_contents = set(
+        os.path.split(item['path'])[1] for item in
+        client.metadata('/Easter basket').get('contents'))
 
-    # # check if /Easter basket has all five eggs
-    basket_contents = client.metadata('/Easter basket').get('contents')
-    found_eggs_counter = 0
-    for item in basket_contents:
-        if os.path.split(item['path'])[1] in file_names:
-            found_eggs_counter += 1
-
-    if found_eggs_counter == 5:
+    if basket_contents >= set(egg_refs.keys()):
         elapsed = time.time() - float(redis_client.hget('start_times', uid))
-        return render_template( 'found_eggs.html', seconds = '%.2f' % elapsed)
+        return render_template('found_eggs.html', seconds=('%.2f' % elapsed))
     else:
-        return render_template('done.html', notyet = True)
-
-def validate_request():
-    '''Validate that the request is properly signed by Dropbox.
-       (If not, this is a spoofed webhook.)'''
-
-    signature = request.headers.get('X-Dropbox-Signature')
-    return signature == hmac.new(APP_SECRET, request.data, sha256).hexdigest()
+        return render_template('done.html', notyet=True)
 
 if __name__=='__main__':
     app.run(debug=True)
